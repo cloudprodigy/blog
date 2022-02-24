59f3a0b5f290929b681f2e6ac43f62a05b934b50
balkaran <ricky@cloudprodigy.ca>
2022-02-24 15:24:55 -0500

    updated scripts

--- scripts/app-install.sh
+++ scripts/app-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 GHOST_VER=4.36.3
 sudo chown -R ubuntu:ubuntu /var/www/ghost
-rm -rf current
-ln -s /var/www/ghost/versions/$GHOST_VER current
+cd /var/www/ghost
+# rm -rf current
+# ln -s /var/www/ghost/versions/$GHOST_VER current
+#sudo find ./ ! -path "./versions/*" -type f -exec chmod 664 {} \;
 cd /var/www/ghost/current/
 npm install
